28ee9ac98e67290ebc9daafb2b55c9dedf58af80
FnuJava <375723803@qq.com>
2017-04-21 15:15:57 +0800

    consumer rest协议还不支持 待修复bug。。

--- boot_dubbo_consumer/src/main/java/com/boot/dubbo/consumer/ConsumerApplication.java
+++ boot_dubbo_consumer/src/main/java/com/boot/dubbo/consumer/ConsumerApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
 @EnableDubboAutoConfiguration
 @ComponentScan("com.boot.dubbo")
 @ImportResource(locations={"classpath:dubbo_consumer.xml","classpath:spring-shiro.xml"})
+@PropertySource("classpath:dubbo.properties")
 public class ConsumerApplication {
   public static void main(String[] args) {
     SpringApplication.run(ConsumerApplication.class, args);
